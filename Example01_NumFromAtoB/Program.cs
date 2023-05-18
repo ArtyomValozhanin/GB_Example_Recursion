0a9013912d2b66a3405108e0685568b43841458c
AVA <Marsota_mixer@tut.by>
2023-05-18 18:44:51 +0300

    added Example01_NumFromAtoB

---
+++ Example01_NumFromAtoB/Program.cs
@@ -0,0 +1,21 @@
+﻿// Собрать строку с числами от a до b, где a <= b
+
+string NumbersFor(int a, int b) // Итеративный подход
+{
+    string result = string.Empty;
+    for (int i = a; i <= b; i++) // i++ инкримент; i-- декримент
+    {
+        result += $"{i} ";
+    }
+    return result;
+}
+
+string NumbersRec(int a, int b) // Рекурсивный подход
+{
+    if (a <= b) return NumbersRec(a + 1, b) + $"{a} ";
+    // для разворота вывода, достаточно поменять местами слогаемые в условии.
+    else return string.Empty;
+}
+
+Console.WriteLine(NumbersFor(1, 10));
+Console.WriteLine(NumbersRec(1, 10));
